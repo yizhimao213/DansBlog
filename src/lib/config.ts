a0c3316a268b2bd38f2b196086c01c8973881421
yizhimao213 <yizhimao726@gmail.com>
2026-08-30 19:29:50 +0800

    评论

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE = import.meta.env.DEV ? 'http://localhost:8787' : 'https://api.danarnoux.com';
+export const API_BASE = import.meta.env.DEV ? 'http://localhost:8787' : 'https://www.ixoxi.cn';
 export const TOKEN_KEY = 'blog_token';
